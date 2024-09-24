5d698c53c5fbed2218bb40c5ec99cae2abbbc930
Maxime Moreillon <m.moreillon@gmail.com>
2024-09-24 16:29:42 +0900

    renamed middleware

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import legacyAuthMiddleware from "@moreillon/express_identification_middleware"
-import passportJwtMiddleware from "@moreillon/express-oidc"
+import oidcMiddleware from "@moreillon/express-oidc"
 
 export const { IDENTIFICATION_URL, OIDC_JWKS_URI } = process.env
 
@@ -15,7 +15,7 @@ export let authMiddleware = (
 if (OIDC_JWKS_URI) {
   console.log(`[Auth] Using OIDC authentication with JWKS URI ${OIDC_JWKS_URI}`)
 
-  authMiddleware = passportJwtMiddleware({ jwksUri: OIDC_JWKS_URI })
+  authMiddleware = oidcMiddleware({ jwksUri: OIDC_JWKS_URI })
 } else if (IDENTIFICATION_URL) {
   console.log(
     `[Auth] Using Legacy authentication with url ${IDENTIFICATION_URL}`
